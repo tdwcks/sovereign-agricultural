82c4fa9f2e81ed857a4cc7f8d80d8a5302f163f1
Adam Wilson <adam@aye-creative.co.uk>
2017-05-31 12:48:13 +0100

    Make aye link open in new tab

--- wp-content/themes/sovereign-agricultural/footer.php
+++ wp-content/themes/sovereign-agricultural/footer.php
@@ -47,7 +47,7 @@
 		<img src="<?= get_template_directory_uri(); ?>/img/footer_grass.png" class="hay-bale">
 		<div class="bottom-bar">
 			<div class="grid-container">
-				<p>&copy; <?php echo date("Y"); ?> Sovereign Agricultural services Limited. all rights reserved. designed by <a href="https://aye.agency">aye.agency</a></p>
+				<p>&copy; <?php echo date("Y"); ?> Sovereign Agricultural services Limited. all rights reserved. designed by <a href="https://aye.agency" target="_blank">aye.agency</a></p>
 			</div>
 		</div>
 	</div>
