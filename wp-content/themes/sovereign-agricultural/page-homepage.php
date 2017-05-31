4608622f83c8be68b2a1c73bf7d430d25201e4f6
Adam Wilson <adam@aye-creative.co.uk>
2017-05-31 13:02:40 +0100

    Centred signature

--- wp-content/themes/sovereign-agricultural/page-homepage.php
+++ wp-content/themes/sovereign-agricultural/page-homepage.php
@@ -85,7 +85,7 @@
 					<?php the_field('quote'); ?>
 					<img src="<?php echo get_site_url(); ?>/wp-content/themes/sovereign-agricultural/img/signature.png">
 					<div class="name">
-						<p>Tim Bennet,<p class="light">Director</p></p>
+						<p>Tim Bennet,<span class="light">Director</span></p>
 					</div>
 				</div>
 			</div>
