9bce2a800b331d132475fe5702f2acac3f1b59cc
Adam Wilson <adam@aye-creative.co.uk>
2017-07-10 17:35:02 +0100

    Wordpress Aye logo and yoast seo to bottom functions

--- wp-content/themes/sovereign-agricultural/functions.php
+++ wp-content/themes/sovereign-agricultural/functions.php
@@ -15,4 +15,30 @@ add_action( 'init', 'register_my_menu' );
 show_admin_bar(false);
 
 add_theme_support( 'post-thumbnails' );
+
+/*-----------------------------------------------------------------------------------*/
+/* Moves Yoast SEO plugin to the bottom
+/*-----------------------------------------------------------------------------------*/
+
+function yoasttobottom() {
+	return 'low';
+}
+add_filter( 'wpseo_metabox_prio', 'yoasttobottom');
+
+/*-----------------------------------------------------------------------------------*/
+/* Aye! Login
+/*-----------------------------------------------------------------------------------*/
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/aye-colour.svg);
+            background-size: 120px;
+            height: 65px;
+            width: 120px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
 ?>
