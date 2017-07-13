f598cb20a99d86ac67567457c017c5ee62a51703
Adam Wilson <adam@aye-creative.co.uk>
2017-07-13 13:50:22 +0100

    Optimised header JS

--- wp-content/themes/sovereign-agricultural/footer.php
+++ wp-content/themes/sovereign-agricultural/footer.php
@@ -55,5 +55,20 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/plugins/swiper.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/global.js"></script>
 	<?php wp_footer(); ?>
+
+	<script src="https://use.fontawesome.com/b94ee9d7cf.js"></script>
+
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-100138649-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
+
+	<script src='https://api.mapbox.com/mapbox-gl-js/v0.36.0/mapbox-gl.js'></script>
+	<link href='https://api.mapbox.com/mapbox-gl-js/v0.36.0/mapbox-gl.css' rel='stylesheet' />
 	</body>
 </html>
--- wp-content/themes/sovereign-agricultural/header.php
+++ wp-content/themes/sovereign-agricultural/header.php
@@ -23,9 +23,6 @@
 	    <meta name="msapplication-config" content="<?php bloginfo('stylesheet_directory'); ?>/favicons/browserconfig.xml">
 	    <meta name="theme-color" content="#ffffff">
 
-		<script src='https://api.mapbox.com/mapbox-gl-js/v0.36.0/mapbox-gl.js'></script>
-		<link href='https://api.mapbox.com/mapbox-gl-js/v0.36.0/mapbox-gl.css' rel='stylesheet' />
-
 		<script src="https://use.typekit.net/gce6yyu.js"></script>
 		<script>try{Typekit.load({ async: true });}catch(e){}</script>
 		<!--[if IE]>
@@ -35,19 +32,7 @@
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-		<script src="https://use.fontawesome.com/b94ee9d7cf.js"></script>
-
 		<?php wp_head(); ?>
-
-		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-		  ga('create', 'UA-100138649-1', 'auto');
-		  ga('send', 'pageview');
-		</script>
 	</head>
 
 	<body <?php body_class(); ?>>
